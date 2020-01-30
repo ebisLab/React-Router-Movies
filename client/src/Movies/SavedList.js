c9bd9a105b1646a74d57e8888fbdf21cc545a22f
ebisLab <ebaiden@techie.com>
2020-01-29 19:15:48 -0500

    useHistory to push back home

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -3,19 +3,34 @@ import {Route, Link} from 'react-router-dom';
 import MovieList from './MovieList';
 import {useHistory} from 'react-router-dom'
 
-const SavedList = props => (
+
+
+const SavedList = props => {
+
+  const history = useHistory();
+  console.log('our history', history)
+  const routeBackHome = () =>{
+
+    setTimeout(()=>{
+history.push('/')
+    }, 1000)
+
+  }
+
+  return (
+  
   <div className="saved-list">
     <h3>Saved Movies:</h3>
     {props.list.map(movie => (
       <span className="saved-movie">{movie.title}</span>
     ))}
-    <div className="home-button">
-      {/* Home */}
+    <div className="home-button" onClick={routeBackHome}>
+      Home
       {/* <a href="google.com">Hello World</a> */}
-      <Link to='/'>Home</Link>
+      {/* <Link to='/'>Home</Link> */}
        <Route path="/"/>
       </div>
   </div>
-);
+);}
 
 export default SavedList;
